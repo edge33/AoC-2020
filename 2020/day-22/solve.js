fda7f44325b05af387389b09b79cb4eab27c7668
Francesco Maida <francesco.maida@nttdata.com>
2021-01-16 14:54:38 +0100

    removed comments

--- 2020/day-22/solve.js
+++ 2020/day-22/solve.js
@@ -73,52 +73,24 @@ const playRecursiveCombat = (deck1, deck2) => {
   return computeWinnersScore(winner === 1 ? deck1 : deck2)
 }
 
-let nextGame = [1]
-
 const playGame = (deck1, deck2, previousRounds) => {
-  let g = nextGame[nextGame.length - 1]
-  if (g % 1000 === 0) {
-    console.log(`=== Game ${nextGame[nextGame.length - 1]} ===`)
-  }
-  nextGame.push(g + 1)
-  // console.log()
-  let r = 1
   while (!deck1.isEmpty() && !deck2.isEmpty()) {
-    // console.log('previous rounds in this game', previousRounds)
     if (isAlreadyPlayed(previousRounds, deck1, deck2)) {
-      // console.log('round already played')
       return 1
     }
-    // console.log(`-- Round ${r} (Game ${g}) --`)
     previousRounds.push({ deck1: deck1.slice(), deck2: deck2.slice() })
-    playRound(deck1, deck2, r, g)
-    // console.log()
-    r++
+    playRound(deck1, deck2)
   }
-  let winner = deck1.isEmpty() ? 2 : 1
-  // console.log(`The winner of game ${g} is player ${winner}!`)
-  // console.log()
-  lastGamePlayed = g
   return deck1.isEmpty() ? 2 : 1
 }
 
-const playRound = (deck1, deck2, r, g) => {
-  // console.log("Player 1's deck:", deck1.join(' '))
-  // console.log("Player 2's deck:", deck2.join(' '))
+const playRound = (deck1, deck2) => {
   const player1Card = deck1.dequeue()
   const player2Card = deck2.dequeue()
-  // console.log('Player 1 plays:', player1Card)
-  // console.log('Player 2 plays:', player2Card)
   if (deck1.length >= player1Card && deck2.length >= player2Card) {
-    //recurse
-    // console.log('Playing a sub-game to determine the winner...')
-    // console.log()
-
     let subDeck1 = deck1.slice(0, player1Card)
     let subDeck2 = deck2.slice(0, player2Card)
-    // console.
-    let winner = playGame(subDeck1, subDeck2, [], g + 1)
-
+    let winner = playGame(subDeck1, subDeck2, [])
     if (winner === 1) {
       deck1.enqueue(player1Card)
       deck1.enqueue(player2Card)
@@ -128,13 +100,9 @@ const playRound = (deck1, deck2, r, g) => {
     }
   } else {
     if (player1Card > player2Card) {
-      // console.log(`Player 1 wins round ${r} of game ${g}!`)
-      // console.log()
       deck1.enqueue(player1Card)
       deck1.enqueue(player2Card)
     } else {
-      // console.log(`Player 2 wins round ${r} of game ${g}!`)
-      // console.log()
       deck2.enqueue(player2Card)
       deck2.enqueue(player1Card)
     }
